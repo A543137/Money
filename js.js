84d47654a2a57d8e474faa87a553d19ff323dae3
Andrey2020 <andru29061972@gmail.com>
2020-03-25 18:54:16 +0200

    Cycles JS

--- js.js
+++ js.js
@@ -1,4 +1,4 @@
-let num = 50;
+
 
 if (num < 49) {
     console.log("Неверно!")
@@ -27,4 +27,23 @@ switch (num) {
     default:
         console.log("Что-то пошло не так!");
         break;                
+}
+
+let num = 50;
+ // while (num < 55) {
+ //   console.log(num);
+  //  num++;
+//  }
+
+do {
+    console.log(num);
+    num++;
+}
+while (num < 55);
+
+for (let i = 1; i < 8; i++) {
+    if (i == 6) {
+        break;
+    }
+    console.log(i)
 }
